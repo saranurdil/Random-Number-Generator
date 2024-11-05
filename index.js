0d1b2851f9279131c517c98ef30a63f3cdc63b7c
Sara Nurdil <saranurdil2005@gmail.com>
2024-11-05 14:34:31 -0600

    Created and modified an allowedchars variable which includes all allowed characters based on user's input

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 function generatePassword(length, lowCase, upperCase, numbers, symbols){
 
+    //possible options
     const lowerCaseOptions = "abcdefghijklmnopqrstuvwxyz";
     const upperCaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const numbersOption = "1234567890";
-    const symblOptions = "~`! @#$%^&*()_-+={[}]|\:;'<,>.?/";
+    const symbolOptions = "~`! @#$%^&*()_-+={[}]|\:;'<,>.?/";
 
+    let allowedchars; //includes all allowed characters based on user's input
+    let password;
+
+    // if the user entered true for any of them, all possible options are concatenated to allowedchars
+    allowedchars += lowCase ? lowerCaseOptions : ""; 
+    allowedchars += upperCase ? upperCaseOptions : "";
+    allowedchars += numbers ? numbersOptions : "";
+    allowedchars += symbols ? symbolOptions : "";
 }
