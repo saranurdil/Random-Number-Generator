217b42f1fc52e531a929a0542d33ebc08a809afe
Sara Nurdil <saranurdil2005@gmail.com>
2024-11-05 14:28:04 -0600

    Created a generating function. Created variables with possible characters for a password

---
+++ index.js
@@ -0,0 +1,8 @@
+function generatePassword(length, lowCase, upperCase, numbers, symbols){
+
+    const lowerCaseOptions = "abcdefghijklmnopqrstuvwxyz";
+    const upperCaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbersOption = "1234567890";
+    const symblOptions = "~`! @#$%^&*()_-+={[}]|\:;'<,>.?/";
+
+}
